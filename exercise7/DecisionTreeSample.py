6e35ff5dd0996859f5d16e82b6d211046542fb72
NetoPedro <pedroneto_09@hotmail.com>
2018-09-02 17:24:45 +0300

    Combined 10000 different decision trees in small subsets with 100 instances each, in order to train an approximation to a random forest classifier. The final prediction is based in the mode.

--- exercise7/DecisionTreeSample.py
+++ exercise7/DecisionTreeSample.py
@@ -1,10 +1,13 @@
 import sklearn.datasets as datasets
 import numpy as np
 import pandas
+# Generating the dataset
 samples = 10000
 dataset_X,dataset_y = datasets.make_moons(n_samples=samples, noise=0.4)
 dataset = pandas.DataFrame(dataset_X)
 dataset["label"] = dataset_y
+
+# Function to divide the dataset into train and test set.
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
@@ -15,6 +18,7 @@ def split_train_test(data, test_ratio):
 
 train_set, test_set = split_train_test(dataset, 0.2)
 
+# Decision Tree instantiation and GridSearch fit to find the best combination of hyperparameters
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 
@@ -27,6 +31,22 @@ tree_grid.fit(train_set.drop("label",axis=1), train_set["label"])
 print(tree_grid.best_estimator_)
 print(tree_grid.best_score_)
 
+# Evaluation through prediction on the test_set
 from sklearn.metrics import accuracy_score
-
 print(accuracy_score(test_set["label"], tree_grid.predict(test_set.drop("label",axis=1),)))
+predictions = pandas.DataFrame()
+for i in range(1,10000):
+    sub_decision_tree = DecisionTreeClassifier(max_depth=decision_tree.max_depth,max_leaf_nodes=decision_tree.max_leaf_nodes)
+    shuffled_indices = np.random.permutation(len(train_set))
+    subset = train_set.iloc[shuffled_indices[:100]]
+    sub_decision_tree.fit(subset.drop("label",axis=1),subset["label"])
+    sub_prediction = sub_decision_tree.predict(test_set.drop("label",axis=1))
+    predictions[i] =  sub_prediction
+
+
+from scipy import stats
+
+test_set_t = predictions.values.T
+finalPrediction = stats.mode(test_set_t)[0].astype(int)
+print(finalPrediction.T)
+print(accuracy_score(test_set["label"], finalPrediction.T))
